09a9d10782f3fb374535e7697cec3e2ad9749ae0
Daneel <z69ding@uwaterloo.ca>
2024-05-31 16:56:36 -0400

    revision as requested

--- Range_List.py
+++ Range_List.py
@@ -6,14 +6,14 @@
 #              the value of y changes. 
 # Method: To achieve this, the real data is the augmentation of stored data. 
 #         For example, for data [[x1,y1], [x2,y2], [x3,y3]], it is stored as [[x1,y1], [x2,y2-y1], [x3, y3-y2]]
-#         When call function "show", the shown y(x) is the augmentation of the stored data before x
+#         When call function "code", the shown y(x) is the augmentation of the stored data before x
 # Notes: both x and y are floats
 
 class range_list():
     
     def __init__(self): # initialization
         self.dict = {}
-
+        print('Start:[]')
         
     def add(self, start, end, amount): # to increase the value of the range [from, to) by a given amount
     # method: in the stored data, we need to increase the y at x=from by amount and decrease the y at x=end by -amount
@@ -26,42 +26,56 @@ class range_list():
             self.dict.update({str(end): -amount})
         else:
             self.dict[str(end)] += -amount
+        print(f'Call: add({start},{end},{amount}) ==> ',end='')
+        self.show()
             
     def set(self, start, end, amount): # to set the value of the range [from, to) to a given amount
-        # get the real data
-        result = self.show()
-        # if x=from exists in the data, set y at x=from to amount; if not, create (x, amount)
-        if start in list(result.keys()):
-            result[str(start)] = amount
+        # if the data is empty:
+        if self.dict == {}:
+            result = {str(start): amount, str(end): 0}
         else:
-            result.update({str(start): amount}) 
-        # if x=to does exist in the data, not need to change; if not, find the y at x=to
-        if str(end) not in list(result.keys()): 
-            result.update({str(end): '_'})
-        for i in sorted(list(result.keys())):
-            if float(i) > start and float(i) < end:
-                holder = result[i]
-                del result[i]
+            # get the real data
+            result = self.decode()
+            # if x=from exists in the data, set y at x=from to amount; if not, create (x, amount)
+            if start in list(result.keys()):
+                result[str(start)] = amount
+            else:
+                result.update({str(start): amount}) 
+            #delete the data point between x=start and x=end
+            for i in sorted(list(result.keys())):
+                if float(i) > start and float(i) < end:
+                    holder = result[i]
+                    del result[i]
+            # if x=to does exist in the data, not need to change; if not, find the y at x=to
+            if str(end) not in list(result.keys()): 
+                result.update({str(end): '_'})
                 result[str(end)] = holder
-        # output the result
-        result = dict(sorted(result.items()))
+            # output the result
+            result = dict(sorted(result.items()))
         # update the stored data
-        self.dict = reverse_show(result) 
-
+        self.dict = self.code(result)
+        print(f'Call: set({start},{end},{amount}) ==> ',end='')
+        self.show()
         
-    def show(self): # to convert the stored data to the real data and show it
+    def decode(self): # to decode the stored data to the real data (the data cannot be empty at this time)
         sorted_dict = dict(sorted(self.dict.items())) # make the dictionary in order according to the key
         key_list = sorted(list(self.dict.keys())) # an ordered list of all keys
-        result = {}# initilization, the first pair in the result dictionary
-        for i in range(len(self.dict)):
-            result.update({key_list[i]:sum(self.dict[key_list[j]] for j in range(i+1))})
-        print("The range list is",list([float(i[0]),i[1]] for i in result.items()))
+        result = {key_list[0]: self.dict[key_list[0]]}# initialization, the first pair in the result dictionary
+        for i in range(1,len(self.dict)):
+            new_key = key_list[i]
+            result.update({new_key: self.dict[new_key] + result[key_list[i-1]]})
         return result
     
-def reverse_show(norm_form): # # to convert the real data to the format of the stored data
-    key_list = sorted(list(norm_form.keys())) # an ordered list of all keys
-    result = {key_list[0]:norm_form[key_list[0]]}# initilization, the first pair in the result dictionary
-    for i in range(1,len(norm_form)):
-        result.update({key_list[i]:(norm_form[key_list[i]] - norm_form[key_list[i-1]])})
-    return result
 
+    def code(self,data): # # to code the real data to the format of the stored data
+        key_list = sorted(list(data.keys())) # an ordered list of all keys
+        result = {key_list[0]:data[key_list[0]]}# initilization, the first pair in the result dictionary
+        for i in range(1,len(data)):
+            result.update({key_list[i]:(data[key_list[i]] - data[key_list[i-1]])})
+        return result
+    
+    def show(self): #print the data in the form of [[x1,y1],[x2,y2],[x3,y3]]
+        print(list([float(i[0]),i[1]] for i in self.decode().items()))
+        
+    def result(self): #the data as a dictionary
+        return list([float(i[0]),i[1]] for i in self.decode().items())
--- Test.py
+++ Test.py
@@ -1,21 +1,30 @@
+import unittest
 from Range_List import range_list
-# How to use:
-# X = range_list(), to initialize, now X is empty expressing y is always 0
-# X.add(from ,to, amount), to add an amount to the y where x in range [from, to)
-# X.set(from ,to, amount), to set the value of y to amount where x in range [from, to)
 
+class TestFunction(unittest.TestCase):
+    def test_1(self):
+        X = range_list()
+        X.add(10, 30, 1)
+        X.add(20, 40, 1)
+        X.add(10, 40, -2)
+        self.assertEqual(X.result(), [[10.0, -1], [20.0, 0], [30.0, -1], [40.0, 0]])
+    def test_2(self):
+        X = range_list()
+        X.add(10, 30, 1)
+        X.add(20, 40, 1)
+        X.add(10, 40, -1)
+        X.add(10, 40, -1)
+        self.assertEqual(X.result(), [[10.0, -1], [20.0, 0], [30.0, -1], [40.0, 0]])
+    def test_3(self):
+        X = range_list()
+        X.set(10, 30, 1)
+        X.add(20, 40, 1)
+        X.set(10, 40, -1)
+        X.add(10, 40, -1)
+        self.assertEqual(X.result(), [[10.0, -2], [40.0, 0]])
 
-## Test Example
-#Test 1
-X = range_list()
-X.add(10, 30, 1)
-X.add(20, 40, 1)
-X.add(10, 40, -2)
-X.show()
-
-#Test 2
-X = range_list()
-X.add(11, 30.5, 1)
-X.add(20.1, 45, 1)
-X.add(10, 45, -2)
-X.show()
+if __name__ == '__main__':
+    #import time
+    #start = time.time()
+    unittest.main(argv=['first-arg-is-ignored'], exit=False)
+    #print(f'runtime: {time.time() - start:.20f}')
